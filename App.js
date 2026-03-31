cf8185d72f789d8c47edab93f6e0b776ec7d9df2
anas221b <anasanas0401@gmail.com>
2026-03-31 23:08:30 +0530

    creating component

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child", key: "h1-child" }, "i am h1 tag"),
-
-  React.createElement("div", { id: "tag", key: "h2-child" }, "h2 tag"),
-]); //wn u wnat to create to child in a div we use []
+const Title = () => <h1 className="head">namaste jsx</h1>;
+const num = 1000;
+const Heading = () => (
+  <div id="container">
+    <Title />
+    {num}
+    <h1 className="heading"> namaaste functional component</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<Heading />);
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 npx parcel build index.html ---- production build
+npx parcel index.html ----- local host
+npm start---- short cut
